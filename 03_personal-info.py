bc512a8ebe81516ac80a3c0535812b1c17e3d8cf
spacejam13 <80799619+spacejam13@users.noreply.github.com>
2022-02-01 14:32:57 -0500

    Delete 03_personal-info.py

--- 03_personal-info.py
+++
@@ -1,7 +0,0 @@
-firstName = input("What is your first name? ")
-lastName = input("What is your last name? ")
-location = input("What is your location? ")
-age = input("What is your age? ")
-space = " "
-print("Hi " + firstName + space + lastName + "! Your location is " + location + " and you are " + age + " years old.")
-
